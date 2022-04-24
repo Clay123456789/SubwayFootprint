2b8cd0be2196109bc7f55d9d0eb64c278e355955
13552360699 <1357748191@qq.com>
2022-04-24 15:00:45 +0800

    activity 可传参导航，已引入高德sdk

--- app/src/main/java/com/dominate_orientation/subwayfootprint/MainActivity.java
+++ app/src/main/java/com/dominate_orientation/subwayfootprint/MainActivity.java
@@ -3,11 +3,14 @@ package com.dominate_orientation.subwayfootprint;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "lol wtf";
     public TextView num1 ;
     public TextView num2 ;
 
@@ -26,4 +29,15 @@ public class MainActivity extends AppCompatActivity {
     public void aMethod2(View view){
         Navigation.findNavController(view).navigate(R.id.action_frag_2_to_frag_1);
     }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
