dc10ad75e03ee2a13e3236daf8088f697abed0a6
xsbsc <1249171883@qq.com>
2022-04-29 17:51:30 +0800

    找回密码部分完成

--- app/src/main/java/com/dominate_orientation/subwayfootprint/ui/login/LoginActivity.java
+++ app/src/main/java/com/dominate_orientation/subwayfootprint/ui/login/LoginActivity.java
@@ -25,6 +25,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.dominate_orientation.subwayfootprint.Find_PasswordActivity;
 import com.dominate_orientation.subwayfootprint.PersonalcenterActivity;
 import com.dominate_orientation.subwayfootprint.R;
 import com.dominate_orientation.subwayfootprint.RegisterActivity;
@@ -207,6 +208,11 @@ public class LoginActivity extends AppCompatActivity {
                     passwordEditText.getText().toString());
             message = "失败";
         }
+    }
+
+    public void findPW(View view){
+        Intent intent =new Intent(LoginActivity.this, Find_PasswordActivity.class);
+        startActivity(intent);
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
