a363b2928c435c2d85a2646479d6cc921d87f89c
zhuyihao <zyh13325038777@outlook.com>
2022-05-01 16:30:42 +0800

    用户宝箱

--- app/src/main/java/com/dominate_orientation/subwayfootprint/Treasure_user.java
+++ app/src/main/java/com/dominate_orientation/subwayfootprint/Treasure_user.java
@@ -4,17 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.alibaba.fastjson.JSONObject;
-import com.dominate_orientation.subwayfootprint.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.LinkedList;
 
 import okhttp3.Call;
 import okhttp3.FormBody;
@@ -22,81 +21,38 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-
-public class Tanjifen_main extends AppCompatActivity {
-    private Button mButton1 ;
-    private Button mButton2 ;
-    private Button mButton3;
-    private TextView textView;
-    private Intent intent;
-
+public class Treasure_user extends AppCompatActivity {
+    private ListView mLv1=null;
     private OkHttpClient okHttpClient;
+    private Intent intent;
     private  String s;
+    private  LinkedList<User_treasure> mData;
+
     public void setS(String s) {
         this.s = s;
-    }
-
-    public String getS() {
-        return s;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tanjifen_mian);
+        setContentView(R.layout.activity_treasure_user);
+
         okHttpClient=new OkHttpClient();
 
-       textView=findViewById(R.id.tv_3);
-
-        String json= null;
+        String json1= null;
         try {
-            json = postSync("https://thelittlestar.cn:8088/user/getUser");
+            json1 = postSync("https://thelittlestar.cn:8088/treasure/getUserTreasure");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        JSONObject jsonObject = JSONObject.parseObject(json);  //接收到所有传过来的json数据
+        JSONObject jsonObject = JSONObject.parseObject(json1);  //接收到所有传过来的json数据
         String  data = jsonObject.getString("data");     //用String接收json数据中的data数据
+        Type type=new TypeToken<LinkedList<User_treasure>>(){}.getType();
         Gson gson=new Gson();
-        User user= gson.fromJson(data, User.class);
-
-
-
-        textView.setText(user.getCredit());
-
-
-        mButton1=findViewById(R.id.Tbutton_1);
-        mButton1.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view1) {
-        //碳积分历史页面
-        Intent intent= new Intent(Tanjifen_main.this, Tanjifen_history.class);
-        startActivity(intent);
-    }
-});
-
-        mButton2=findViewById(R.id.Tbutton_2);
-        mButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view2) {
-                //碳积分排行页面
-                Intent intent= new Intent(Tanjifen_main.this,Tanjifen_rank.class);
-                startActivity(intent);
-            }
-        });
-        mButton3=findViewById(R.id.Tbutton_3);
-        mButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view3) {
-                //使用说明页面
-                Intent intent= new Intent(Tanjifen_main.this,Tanjifen4.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-
-
+        mData = gson.fromJson(data,type);
+      System.out.println(json1);
+        mLv1 =  findViewById(R.id.lv_2);
+        mLv1.setAdapter(new TreasureUserAdapter(mData,Treasure_user.this));
 
 
 
@@ -105,44 +61,48 @@ public class Tanjifen_main extends AppCompatActivity {
         bottomNavigationView.setOnItemSelectedListener((menuItem)->{
             switch (menuItem.getItemId()){
                 case R.id.nav_home:
-                    intent =new Intent(Tanjifen_main.this, main_page.class);
+                    intent =new Intent(Treasure_user.this, main_page.class);
                     startActivity(intent);
                     break;
                 case R.id.nav_credit:
-                    intent =new Intent(Tanjifen_main.this,Tanjifen_main.class);
+                    intent =new Intent(Treasure_user.this,Tanjifen_main.class);
                     startActivity(intent);
                     break;
                 case R.id.nav_shop:
-                    intent =new Intent(Tanjifen_main.this, MainActivity.class);
+                    intent =new Intent(Treasure_user.this, PersonalcenterActivity.class);
                     startActivity(intent);
                     break;
                 case R.id.nav_person:
-                    intent =new Intent(Tanjifen_main.this, PersonalcenterActivity.class);
+                    intent =new Intent(Treasure_user.this, PersonalcenterActivity.class);
                     startActivity(intent);
                     break;
             }
             return true;
         });
 
-
     }
 
     public String postSync(String url) throws InterruptedException {
 
-        Thread threadB= new Thread(){
+        Thread threadA= new Thread(){
 
             @Override
+
             public void run(){
                 FormBody formBody=new FormBody.Builder().build();
+
                 Token app = (Token)getApplicationContext();
                 String token = null;
                 token =app.getToken();
                 Request request=new Request.Builder().url(url).post(formBody).header("token", token).build();
 
+
+
                 Call call=okHttpClient.newCall(request);
                 try {
                     Response response=call.execute();
                     setS(response.body().string());
+
 
 
                 } catch (IOException e) {
@@ -151,9 +111,11 @@ public class Tanjifen_main extends AppCompatActivity {
             }
 
         };
-        threadB.start();
-        threadB.join();
+        threadA.start();
+        threadA.join();
+
 
         return  s;
     }
+
 }
---
+++ app/src/main/java/com/dominate_orientation/subwayfootprint/User_treasure.java
@@ -0,0 +1,103 @@
+package com.dominate_orientation.subwayfootprint;
+
+public class User_treasure {
+    private String tid;
+    private String variety;
+    private String content;
+    private String credit;
+    private String pid;
+    private String fromdate;
+    private String status;
+    private String uid;
+    private String mid;
+    private String uid2;
+    private String gatedate;
+
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+
+    public String getVariety() {
+        return variety;
+    }
+
+    public void setVariety(String variety) {
+        this.variety = variety;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCredit() {
+        return credit;
+    }
+
+    public void setCredit(String credit) {
+        this.credit = credit;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getFromdate() {
+        return fromdate;
+    }
+
+    public void setFromdate(String fromdate) {
+        this.fromdate = fromdate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public String getUid2() {
+        return uid2;
+    }
+
+    public void setUid2(String uid2) {
+        this.uid2 = uid2;
+    }
+
+    public String getGatedate() {
+        return gatedate;
+    }
+
+    public void setGatedate(String gatedate) {
+        this.gatedate = gatedate;
+    }
+}
