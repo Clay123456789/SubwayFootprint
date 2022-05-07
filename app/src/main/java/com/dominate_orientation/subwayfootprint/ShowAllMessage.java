4a9fe2a9aa38d46e0ef89f1a8e533a3aebfbdf54
YO-LO-vv <919440676@qq.com>
2022-05-07 23:36:33 +0800

    fix show messages

--- app/src/main/java/com/dominate_orientation/subwayfootprint/ShowAllMessage.java
+++ app/src/main/java/com/dominate_orientation/subwayfootprint/ShowAllMessage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -85,5 +86,10 @@ public class ShowAllMessage extends AppCompatActivity {
 
 
 
+    }
+
+    public void go_back_to_route(View view)
+    {
+        finish();
     }
 }
